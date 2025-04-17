fc4891467b6e929e0f00d8c03ceee25f3b7c882e
Azzaxy <abdurrohmanazis@gmail.com>
2025-04-17 14:25:51 +0700

    feat: add sign-in screen

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,43 @@
-import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import images from "@/constants/images";
+import icons from "@/constants/icons";
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="h-full bg-white">
+      <ScrollView contentContainerClassName="h-full">
+        <Image
+          source={images.onboarding}
+          className="w-full h-4/6"
+          resizeMode="contain"
+        />
+        <View className="px-10">
+          <Text className="text-base text-center uppercase text-black-200 font-rubik">
+            Welcome To Restate
+          </Text>
+          <Text className="mt-2 text-3xl text-center text-black-300 font-rubik-bold">
+            Let's Get You Closer to {"\n"}{" "}
+            <Text className="text-primary-300">Your Ideal Home</Text>
+          </Text>
+          <View className="mt-8">
+            <Text className="text-lg text-center font-rubik text-black-200">
+              Login to ReState with Google
+            </Text>
+            <TouchableOpacity className="w-full px-2 py-3 mt-5 text-center bg-white rounded-full shadow-md">
+              <View className="flex flex-row items-center justify-center">
+                <Image source={icons.google} className="w-6 h-6" />
+                <Text className="ml-2 text-lg font-rubik-medium text-black-300">
+                  Continue with Google
+                </Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
