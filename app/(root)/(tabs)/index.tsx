6e576a8328c4983e839dbe17cf20cbb5ff247b74
Azzaxy <abdurrohmanazis@gmail.com>
2025-04-21 05:18:17 +0700

    feat: update Index component to use FlashList for rendering cards; enhance Card component with props for dynamic data

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -14,82 +14,90 @@ const Index = () => {
 
   return (
     <SafeAreaView className="flex-1 bg-white">
-      <ScrollView>
-        <View className="px-5">
-          <View className="flex flex-row items-center justify-between mt-5">
-            <View className="flex flex-row items-center justify-between w-full">
-              <View className="flex-row items-center ">
-                <Image
-                  source={{ uri: user?.avatar }}
-                  className="rounded-full size-12"
-                />
-                <View className="flex-col items-start ml-3">
-                  <Text className="text-black-100">Good Morning</Text>
-                  <Text className="text-lg font-rubik-medium">
-                    {user?.name}
+      <FlashList
+        data={cards}
+        renderItem={({ item }) => (
+          <View className="flex gap-5 mx-2">
+            <Card {...item} />
+          </View>
+        )}
+        estimatedItemSize={100}
+        numColumns={2}
+        contentContainerStyle={{ paddingBottom: 100 }}
+        keyExtractor={(_, index) => index.toString()}
+        ListHeaderComponentStyle={{ display: "flex", gap: 10 }}
+        showsVerticalScrollIndicator={false}
+        ListHeaderComponent={
+          <View className="px-5">
+            <View className="flex flex-row items-center justify-between mt-5">
+              <View className="flex flex-row items-center justify-between w-full">
+                <View className="flex-row items-center ">
+                  <Image
+                    source={{ uri: user?.avatar }}
+                    className="rounded-full size-12"
+                  />
+                  <View className="flex-col items-start ml-3">
+                    <Text className="text-black-100">Good Morning</Text>
+                    <Text className="text-lg font-rubik-medium">
+                      {user?.name}
+                    </Text>
+                  </View>
+                </View>
+                <View>
+                  <Image source={icons.bell} className="size-6" />
+                </View>
+              </View>
+            </View>
+            <Search />
+
+            {/* Featured Section */}
+            <View className="my-5 ">
+              <View className="flex-row items-center justify-between">
+                <Text className="text-xl font-rubik-bold text-black-300">
+                  Featured
+                </Text>
+                <TouchableOpacity>
+                  <Text className="text-base text-primary-300 font-rubik-bold">
+                    See All
                   </Text>
-                </View>
-              </View>
-              <View>
-                <Image source={icons.bell} className="size-6" />
+                </TouchableOpacity>
               </View>
             </View>
-          </View>
-          <Search />
 
-          {/* Featured Section */}
-          <View className="my-5 ">
-            <View className="flex-row items-center justify-between">
-              <Text className="text-xl font-rubik-bold text-black-300">
-                Featured
-              </Text>
-              <TouchableOpacity>
-                <Text className="text-base text-primary-300 font-rubik-bold">
-                  See All
+            {/* Our Recommendation */}
+            <View className="flex-row items-center justify-between gap-5">
+              <FlashList
+                data={featuredCards}
+                horizontal
+                estimatedItemSize={200}
+                keyExtractor={(item) => item.title}
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{ paddingRight: 20 }}
+                className="gap-5"
+                renderItem={({ item }) => (
+                  <View className="flex gap-10 mr-5">
+                    <FeaturedCard {...item} />
+                  </View>
+                )}
+              />
+            </View>
+
+            <View className="my-5 ">
+              <View className="flex-row items-center justify-between">
+                <Text className="text-xl font-rubik-bold text-black-300">
+                  Our Recommendation
                 </Text>
-              </TouchableOpacity>
-            </View>
-          </View>
+                <TouchableOpacity>
+                  <Text className="text-base text-primary-300 font-rubik-bold">
+                    See All
+                  </Text>
+                </TouchableOpacity>
+              </View>
 
-          {/* Our Recommendation */}
-          <View className="flex-row items-center justify-between gap-5">
-            <FlashList
-              data={featuredCards}
-              horizontal
-              estimatedItemSize={200}
-              keyExtractor={(item) => item.title}
-              showsHorizontalScrollIndicator={false}
-              contentContainerStyle={{ paddingRight: 20 }}
-              className="gap-5"
-              renderItem={({ item }) => (
-                <View className="gap-10 mr-5">
-                  <FeaturedCard {...item} />
-                </View>
-              )}
-            />
-          </View>
-
-          <View className="my-5 ">
-            <View className="flex-row items-center justify-between">
-              <Text className="text-xl font-rubik-bold text-black-300">
-                Our Recommendation
-              </Text>
-              <TouchableOpacity>
-                <Text className="text-base text-primary-300 font-rubik-bold">
-                  See All
-                </Text>
-              </TouchableOpacity>
+              <Filters />
             </View>
 
-            <Filters />
-
-            <View className="flex flex-row gap-5 mt-5">
-              <Card />
-              <Card />
-            </View>
-          </View>
-
-          {/* <FlashList
+            {/* <FlashList
           data={cards}
           estimatedItemSize={200}
           renderItem={({ item }) => (
@@ -97,8 +105,9 @@ const Index = () => {
             </View>
           )}
         /> */}
-        </View>
-      </ScrollView>
+          </View>
+        }
+      />
     </SafeAreaView>
   );
 };
--- components/Cards.tsx
+++ components/Cards.tsx
@@ -55,24 +55,36 @@ export const FeaturedCard = ({
   );
 };
 
-export const Card = () => {
+export const Card = ({
+  image,
+  location,
+  title,
+  price,
+  rating,
+  onPress,
+}: Props) => {
   return (
-    <TouchableOpacity className="relative flex-1 w-full px-4 py-4 mt-5 bg-white rounded-lg shadow-sm shadow-black-100/50">
+    <TouchableOpacity
+      className="relative flex-1 w-full px-4 py-4 mt-5 bg-white rounded-lg shadow-sm shadow-black-100/50"
+      onPress={onPress}
+    >
       <View className="absolute z-50 flex flex-row items-center p-1 px-2 rounded-full top-5 right-5 bg-white/90">
         <Image source={icons.star} className="size-2.5" />
-        <Text className="text-primary-300 font-rubik-bold ml-0.5">4.4</Text>
+        <Text className="text-primary-300 font-rubik-bold ml-0.5">
+          {rating}
+        </Text>
       </View>
 
-      <Image source={images.newYork} className="w-full h-40 rounded-lg" />
+      <Image source={image} className="w-full h-40 rounded-lg" />
 
       <View className="flex flex-col mt-2">
         <Text className="text-base text-black-300 font-rubik-bold">
-          Japan Tour
+          {title}
         </Text>
-        <Text className="text-xs font-rubik text-black-300">Tokyo, Japan</Text>
+        <Text className="text-xs font-rubik text-black-300">{location}</Text>
         <View className="flex flex-row items-center justify-between w-full gap-2 mt-1">
           <Text className="text-base font-rubik-bold text-primary-300">
-            Rp. 1.500.000
+            Rp. {price}
           </Text>
           <Image
             source={icons.heart}
