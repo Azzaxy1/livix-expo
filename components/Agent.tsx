63ce18a9bbbea066fff7c2add72918b2947590c2
Azzaxy <abdurrohmanazis@gmail.com>
2025-04-25 14:04:45 +0700

    feat: refactor Agent component layout and styling for improved presentation

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -8,26 +8,29 @@ const Agent = ({ property }: Property) => {
   if (!property?.agent) return null;
 
   return (
-    <View className="px-5 py-4 mt-5 bg-white rounded-lg shadow-sm shadow-black-100/50">
-      <Text className="text-xl font-rubik-semibold text-black-300">Agent</Text>
-      <View className="flex flex-row items-center gap-3 mt-2">
-        <Image
-          source={{ uri: property?.agent?.avatar }}
-          className="rounded-full size-[60px]"
-        />
+    <View className="w-full mt-5 border-t border-primary-200 pt-7">
+      <Text className="text-xl text-black-300 font-rubik-bold">Agent</Text>
 
-        <View>
-          <Text className="text-lg font-rubik-semibold text-black-300">
-            {property?.agent?.name}
-          </Text>
-          <Text className="text-base font-rubik text-black-200">
-            {property?.agent?.email}
-          </Text>
+      <View className="flex flex-row items-center justify-between mt-4">
+        <View className="flex flex-row items-center">
+          <Image
+            source={{ uri: property?.agent.avatar }}
+            className="rounded-full size-14"
+          />
+
+          <View className="flex flex-col items-start justify-center ml-3">
+            <Text className="text-lg text-black-300 text-start font-rubik-bold">
+              {property?.agent.name}
+            </Text>
+            <Text className="text-sm text-black-200 text-start font-rubik-medium">
+              {property?.agent.email}
+            </Text>
+          </View>
         </View>
 
-        <View className="flex flex-row items-center gap-3 ml-auto">
-          <Image source={icons.chat} className="size-6" />
-          <Image source={icons.phone} className="size-6" />
+        <View className="flex flex-row items-center gap-3">
+          <Image source={icons.chat} className="size-7" />
+          <Image source={icons.phone} className="size-7" />
         </View>
       </View>
     </View>
